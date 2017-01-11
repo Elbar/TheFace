4d1d88bc0bf7ec442ca002332cefd3d23e18dc64
katalyzator <web.coder96@gmail.com>
2017-01-11 14:15:24 +0600

    update Actor class in Model

--- actors/admin.py
+++ actors/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Actor, ActorsImage
+
+
+class ActorAdmin(admin.ModelAdmin):
+    search_fields = ['identify']
+    list_display = ['identify']
+    list_filter = ['sex', 'timestamp', 'age', 'weight', 'height']
+    readonly_fields = ['updated', 'timestamp']
+
+    class Meta:
+        model = Actor
+
+
+class ActorsImageAdmin(admin.ModelAdmin):
+    class Meta:
+        model = ActorsImage
+
+
+admin.site.register(Actor, ActorAdmin)
+
+admin.site.register(ActorsImage, ActorsImageAdmin)
--- actors/models.py
+++ actors/models.py
@@ -1,5 +1,80 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+from actors.helper import transform
+
+SEX = (('Male', 'Male'),
+       ('Female', 'Female'),
+       )
+
+READY_TO_GO = (
+    ('Yes', 'Yes'),
+    ('No', 'No'),
+)
+
+READY_TO_MASS = (
+    ('Yes', 'Yes'),
+    ('No', 'No'),
+)
+
+LANGUAGE = (
+    ('English', 'English'),
+    ('Russian', 'Russian'),
+    ('Kyrgyz', 'Kyrgyz'),
+)
+
+STATUS = (
+    ('VIP', 'VIP'),
+    ('BASIC', 'BASIC')
+)
+
+PATH_ACTORS = 'actors/images'
+
+
+class Actor(models.Model):
+    class Meta:
+        verbose_name_plural = 'Добавление Актеров'
+        verbose_name = 'Добавление актеров'
+
+    name = models.CharField(max_length=255, verbose_name='Имя Актера')
+    surname = models.CharField(max_length=255, verbose_name='Фамилия Актера')
+    age = models.IntegerField(verbose_name='Возраст')
+    height = models.CharField(max_length=255, verbose_name='Рост')
+    weight = models.CharField(max_length=255, verbose_name='Вес')
+    identify = models.CharField(max_length=255, verbose_name='Идентификатор')
+    phone_number = models.CharField(max_length=255, verbose_name='Номер Телефона')
+
+    link = models.CharField(max_length=255, verbose_name='ссылка')
+    sex = models.CharField(choices=SEX, verbose_name='Пол', max_length=255)
+
+    language = models.CharField(choices=LANGUAGE, verbose_name='Знание языков', max_length=255)
+    ready_to_go = models.CharField(choices=READY_TO_GO, max_length=255, verbose_name='Готовность к выезду')
+    ready_to_mass = models.CharField(choices=READY_TO_MASS, max_length=255, verbose_name='Готовность к массовым ролям')
+    image = models.FileField(upload_to=transform(PATH_ACTORS), verbose_name='Фотография актера')
+    status = models.CharField(choices=STATUS, max_length=255, verbose_name='Статус Актера')
+
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return self.identify
+
+    def get_absolute_url(self):
+        return "/actors/%i/" % self.id
+
+
+class ActorsImage(models.Model):
+    class Meta:
+        verbose_name_plural = 'Фотографии Актеров'
+        verbose_name = 'Фотографии Актеров'
+
+    actor = models.ForeignKey(Actor, verbose_name='Выберите актера')
+    image = models.ImageField(upload_to=transform(PATH_ACTORS), verbose_name='Фотография Актера')
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return str(self.id)
